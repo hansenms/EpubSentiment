17a2e41c53ce17e9af6ab06c6c7efef7781b30d3
Michael Hansen <mihansen@microsoft.com>
2017-08-26 10:08:05 -0400

    Add project files.

---
+++ EpubSentiment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EpubSentiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
